5e24a20fb89ab5040a7472775b85df30ecb75645
Charles Hill <chill117@users.noreply.github.com>
2017-03-07 21:07:38 +0100

    Update trees.js

--- server/controllers/api/trees.js
+++ server/controllers/api/trees.js
@@ -1,23 +1,10 @@
 'use strict';
 
 module.exports = function(app) {
-	
-	var bodyParser = require('body-parser')
-	var jsonParser = bodyParser.json()
-	var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-	app.use(bodyParser.urlencoded({ extended: false }))
-	app.use(bodyParser.json())
-	
-	app.get('/api/trees', function (req, res) {
-  		
-  		if (!req.body) return res.sendStatus(400);
+	app.get('/api/v1/trees', function (req, res) {
 
-  		var requested = req.body;
-
-  		res.header("Access-Control-Allow-Origin", "*");
-
-		app.db.select([
+		app.db.models.Trees.query().select([
 			'trees.id',
 			'trees.latitude',
 			'trees.longitude',
@@ -25,20 +12,7 @@ module.exports = function(app) {
 			'trees.description',
 			'trees.filename'
 		])
-		.from('trees')
-		.where ('latitude','>',req.body.upperleft_la)
-		.andWhere ('latitude','<',req.body.lowerright_la)
-		.andWhere ('longitude','>',req.body.upperleft_lo)
-		.andWhere ('longitude','<',req.body.lowerright_lo) 
 		.then(function(trees) {
-			console.log('inside then of knex');
-			console.log(trees[0]);
 			res.json(trees);
-			
-			res.send();
-		}).catch(console.log);
-  		
-  		})
-
+		}).catch(next);
 };
-
