e227e14e663ac3540e7cf8cf63d20dc1cb3c0c98
BK <boro.kovac@gmail.com>
2017-04-04 01:02:48 +0200

    basic Vue scaffolding

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,19 +1,19 @@
 document.addEventListener('deviceready', function() {    // The device is ready
 
-    var newTreeButton = document.getElementById('new-tree');
+    // var newTreeButton = document.getElementById('new-tree');
 
-    newTreeButton.addEventListener('click', function() {
-        // Run camera.
-        navigator.camera.getPicture(function(fileURL) {
-            // success
-            uploadPhoto(fileURL);
-        }, function(error) {
-            // error
-            alert(error);
-        }, {
-            destinationType: navigator.camera.DestinationType.FILE_URI
-        });
-    });
+    // newTreeButton.addEventListener('click', function() {
+    //     // Run camera.
+    //     navigator.camera.getPicture(function(fileURL) {
+    //         // success
+    //         uploadPhoto(fileURL);
+    //     }, function(error) {
+    //         // error
+    //         alert(error);
+    //     }, {
+    //         destinationType: navigator.camera.DestinationType.FILE_URI
+    //     });
+    // });
 
     function uploadPhoto(imageURI) {
 
@@ -88,88 +88,78 @@ document.addEventListener('deviceready', function() {    // The device is ready
 
     (function() {
 
-        function getRequestandUpdateTreeDetails(url) {
-          var xhttp = new XMLHttpRequest();
-          xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-              var data = JSON.parse(this.responseText);
-              vm.updateTreeDetails({
-                treeType: data.type,
-                treeDescription: data.description,
-                treeImageURL: data.filename
-              });
-              vm.updateError("");
-            }
-            else if (this.readyState == 4 && this.status == 404) {
-              vm.updateError("There was a problem with your request. Please try again later.");
-              vm.updateTreeDetails({
-                treeType: "",
-                treeDescription: "",
-                treeImageURL: ""
-              });
-            } 
-          };
-          
-          xhttp.open("GET", url, true);
-          xhttp.send();
-        }
+      const customToolbar = {
+      template: '#custom-toolbar',
+      props: ['backLabel']
+      };
 
-
-        const pop = function() {
-          this.pageStack.pop();
-        };
-
-        const customToolbar = {
-          template: '#custom-toolbar',
-          props: ['pop', 'backLabel']
-        };
-
-        const showTreeDetails = {
-          template: '#ShowTreeDetails',
-          methods: { pop },
-          props: ['pageStack', 'treeDescription', 'treeType', 'treeImageURL', 'error'],
-          components: { customToolbar },
-        };
-
-        const welcomePage = {
-          template: '#WelcomePage',
-          methods: {
-            pop,
-            push() {
-              this.pageStack.push(showTreeDetails);
-              this.treeID = this.$refs.treeIDInput.value;
-              getRequestandUpdateTreeDetails("http://localhost:3000/api/v1/tree/" + this.treeID);
-            }
+      const homepage = {
+        template: '#homepage',
+        methods: {
+          add() {
+            this.pageStack.push(confirmPhoto);
           },
-          props: ['pageStack', 'treeID'],
-          components: { customToolbar }
-        };
-
-        var vm = new Vue({
-          el: '.app',
-          template: '#main',
-          data() {
-            return {
-              pageStack: [welcomePage],
-                treeDescription: "Default tree description.",
-                treeType: "Default tree type.",
-                treeImageURL: "Default file URL",
-              treeID: "3", // Default tree ID
-              error: ""
-            };
-          },
-          methods: {
-            updateTreeDetails: function(data) {
-              this.treeDescription = data.treeDescription;
-              this.treeType = data.treeType;
-              this.treeImageURL = data.treeImageURL;
-            },
-            updateError: function(error) {
-              this.error = error;
-            }
-
+          credits(){
+            this.pageStack.push(credits);
           }
-        });
+        },
+        props: ['pageStack'],
+        components: { customToolbar }
+      };      
+
+      const confirmPhoto = {
+        template: '#confirmPhoto',
+        methods: {
+          home(){
+            this.pageStack.pop(homepage);
+          },
+          next(){
+            this.pageStack.push(fillDetails);
+          }
+        },
+        props: ['pageStack'],
+        components: { customToolbar }
+      };
+
+      const fillDetails = {
+        template: '#fillDetails',
+        methods: {
+          home(){
+            this.pageStack.pop(homepage);
+          },
+          save(){
+            this.pageStack.push(treeSaved);
+          }          
+        },
+        props: ['pageStack'],
+        components: { customToolbar }
+      };
+
+
+      const treeSaved = {
+        template: '#treeSaved',
+        props: ['pageStack'],
+        components: { customToolbar }
+      };
+
+      const credits = {
+        template: '#credits',
+        props: ['pageStack'],
+        components: { customToolbar }
+      };
+
+
+      new Vue({
+        el: '#app',
+        template: '#main',
+        data() {
+          return {
+            pageStack: [homepage]
+          };
+        }
+      });
+
+
 
     })();
 
